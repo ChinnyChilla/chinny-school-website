c922301ee09c97e3078f10445b8ac683f0a0d203
Chinny <michaelzheng04@gmail.com>
2020-12-14 14:56:58 -0500

    added style to kenimatics calculator

--- displacement-velocity/displacement-velocity.js
+++ displacement-velocity/displacement-velocity.js
@@ -1,39 +1,45 @@
 window.onload = function () {
-    var calculate = document.getElementById('calculate')
-    var createRow = document.getElementById('createRow')
-    var idCount = 0
+    //all the buttons
+    var calculate = document.getElementById('calculate');
+    var createRow = document.getElementById('createRow');
 
     createRow.onclick = function () {
-        var table = document.getElementById('table1')
+        // all the elements needed for creating new row
+        var table = document.getElementById('table1');
         var selectList = document.createElement("select");
-        var td = document.createElement("td")
-        var tr = document.createElement("tr")
-        selectList.setAttribute('name', "select")
-        selectList.setAttribute('value', "Select One")
-        
-        var optiongroup = document.createElement('optgroup')
-        optiongroup.setAttribute('label', "Simple Directions")
-        selectList.appendChild(optiongroup)
-        var list = new Array("North", "South", "East", "West")
+        var td = document.createElement("td");
+        var tr = document.createElement("tr");
+        selectList.setAttribute('name', "select");
+        selectList.setAttribute('class', 'select');
+        selectList.setAttribute('value', "Select One");
+        //creating new option group with options in array list
+        var optiongroup = document.createElement('optgroup');
+        optiongroup.setAttribute('label', "Simple Directions");
+        selectList.appendChild(optiongroup);
+        var list = new Array("north", "south", "east", "west");
         //Create and append the options
         for (var i = 0; i < list.length; i++) {
-            console.log("in for loop")
             var option = document.createElement("option");
-  
             option.value = list[i];
-            option.text = list[i];
+            option.text = list[i][0].toUpperCase() + list[i].slice(1);
             selectList.appendChild(option);
-        }
-        table.appendChild(selectList)
-        var inputNumber = document.createElement("input")
-        inputNumber.setAttribute('type', 'number')
-        inputNumber.setAttribute('name', 'Quantity')
-        td.appendChild(inputNumber)
-        table.appendChild(td)
-        table.appendChild(tr)
+        };
+        //appends it to the table with all other elements
+        table.appendChild(selectList);
+        var inputNumber = document.createElement("input");
+        inputNumber.setAttribute('type', 'number');
+        inputNumber.setAttribute('name', 'Quantity');
+        inputNumber.setAttribute('class', 'quantity');
+        td.appendChild(inputNumber);
+        table.appendChild(td);
+        table.appendChild(tr);
     }
-    calculate.onclick = function ()   {
-        var distances = document.getElementById('distance')
-        console.log(distances.selectedOptions);
-    }
+    calculate.onclick = function () {
+        var selectedDirection = document.getElementsByClassName('select')
+        var quantities = document.getElementsByClassName('quantity')
+        for (i = 0; i < selectedDirection.length; i++) {
+            console.log(selectedDirection[i].value)
+            console.log(quantities[i].value)
+        };
+    };
 }
